bafc202220b4880fa1b052661f750f1c38de7009
nguyenquan132 <quannguyendinh.221204@gmail.com>
2026-03-10 01:25:59 +0700

    adjust predict_one_image

--- signboard_det/rtdetrv2.py
+++ signboard_det/rtdetrv2.py
@@ -219,6 +219,8 @@ class RTDETRV2Finetuner(pl.LightningModule):
 
         # Postprocessing
         target_size = torch.tensor(pixel_values.shape[2:]).to(self.device) # Resized image
+        if original_size is not None: 
+            original_size = torch.tensor(original_size)
         output = {k: v for k, v in outputs.items()}
         output['pred_logits'] = output.pop('logits')
         results = self.postprocess(output)[0]
